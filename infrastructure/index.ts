0f3ff17702cf9aa39e0546040dcc780828a1d197
hogetest-company <gouhuishe@gmail.com>
2025-07-06 17:10:06 +0900

    ci修正

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,6 +1,6 @@
-import * as pulumi from "@pulumi/pulumi";
+// import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+// import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("my-bucket");
